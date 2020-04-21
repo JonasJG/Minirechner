d85c218e69d9b5f7b4de1f6c0a25894d191761e5
JonasJG <02gufjon@rgtfo-me.it>
2020-04-21 14:50:24 +0200

    Eingabe der Zahlen hinzugefügt

--- Minirechner/Minirechner/Program.cs
+++ Minirechner/Minirechner/Program.cs
@@ -10,6 +10,12 @@ namespace Minirechner
         static void Main(string[] args)
         {
             Console.WriteLine("Dies ist ein kleiner Rechner!");
+            Console.WriteLine("Zahl 1:");
+            Console.ReadLine();
+            Console.WriteLine("Zahl 2:");
+            Console.ReadLine();
+
+            
         }
     }
 }
