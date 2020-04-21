1b5459f45519b403d6950549ba8a547ffccc0598
JonasJG <02gufjon@rgtfo-me.it>
2020-04-21 14:45:33 +0200

    Erste Zeile erstellt

---
+++ Minirechner/Minirechner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minirechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dies ist ein kleiner Rechner!");
+        }
+    }
+}
