533a12e253629210abf9a0b7bcc6f897e40b1dad
JonasJG <02gufjon@rgtfo-me.it>
2020-04-23 14:18:00 +0200

    Programmkopf fertig

--- Minirechner/Minirechner/Program.cs
+++ Minirechner/Minirechner/Program.cs
@@ -10,10 +10,11 @@ namespace Minirechner
         static void Main(string[] args)
         {
             Console.WriteLine("Dies ist ein kleiner Rechner!");
-            Console.WriteLine("Zahl 1:");
+            Console.Write("Zahl 1:");
             int ersteZahl = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("Zahl 2:");
+            Console.Write("Zahl 2:");
             int zweiteZahl=Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("\n\n");
 
             
         }
